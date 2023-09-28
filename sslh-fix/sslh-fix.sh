929a810fb1e25c9601a4360d4298525f22066225
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-09-28 20:24:15 +0700

    Create sslh-fix.sh

---
+++ sslh-fix/sslh-fix.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+clear
+#change directory
+cd /usr/bin
+#remove file
+rm -r sslh-fix-reboot
+rm -rf sslh-fix-reboot
+rm sslh-fix-reboot
+# Download File
+wget -O sslh-fix-reboot "https://raw.githubusercontent.com/arfprsty810/vpn/main/sslh-fix/sslh-fix-reboot.sh"
+#change Permission
+chmod +x sslh-fix-reboot
+# change directory
+cd /root/
+# make nano /etc/rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+# rc.local
+# By default this script does nothing.
+
+sslh-fix-reboot
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
+screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+exit 0
+END
+
+# Ubah izin akses
+chmod +x /etc/rc.local
+
+# enable rc local
+systemctl enable rc-local
+systemctl start rc-local.service
+
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+cd /root/
+echo "5 5 * * * root clearlog && sslh-fix-reboot" >> /etc/crontab
+
+echo -e "Succes fix SSLH error"
+sleep 1
+clear
