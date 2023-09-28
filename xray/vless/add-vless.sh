2cbc40790343a80705a2c0a60f1e93745c905575
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-09-28 20:17:30 +0700

    Create add-vless.sh

---
+++ xray/vless/add-vless.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+clear
+arfvpn="/etc/arfvpn"
+xray="/etc/xray"
+ipvps="/var/lib/arfvpn"
+source ${ipvps}/ipvps.conf
+if [[ "${IP}" = "" ]]; then
+domain=$(cat ${xray}/domain)
+else
+domain=${IP}
+fi
+
+tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"
+until [[ ${user} =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\\E[0;41;36m      Add Xray/Vless Account      \E[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+		read -rp "User: " -e user
+		CLIENT_EXISTS=$(grep -w ${user} ${xray}/config.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+clear
+            echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+            echo -e "\\E[0;41;36m      Add Xray/Vless Account      \E[0m"
+            echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+			echo ""
+			echo "A client with the specified name was already created, please choose another name."
+			echo ""
+			echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+sleep 2
+add-vless
+		fi
+	done
+
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
+exp=`date -d "${masaaktif} days" +"%Y-%m-%d"`
+sed -i '/#vless$/a\#vl# '"${user} ${exp}"'\
+},{"id": "'""${uuid}""'","email": "'""${user}""'"' ${xray}/config.json
+sed -i '/#vlessgrpc$/a\#vl# '"${user} ${exp}"'\
+},{"id": "'""${uuid}""'","email": "'""${user}""'"' ${xray}/config.json
+vlesslink1="vless://${uuid}@${domain}:${tls}?path=/vless&security=tls&encryption=none&type=ws#${user}"
+vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
+vlesslink3="vless://${uuid}@${domain}:${tls}?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
+systemctl restart xray
+clear
+echo -e "----------------------------------" | tee -a /etc/log-create-user.log
+echo -e "\\E[0;41;36m        Xray/Vless Account        \E[0m" | tee -a /etc/log-create-user.log
+echo -e "----------------------------------" | tee -a /etc/log-create-user.log
+echo -e "Remarks   : ${user}" | tee -a /etc/log-create-user.log
+echo -e "Domain    : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "Port TLS  : ${tls}" | tee -a /etc/log-create-user.log
+echo -e "Port NTLS : ${none}" | tee -a /etc/log-create-user.log
+echo -e "Port GRPC : ${tls}" | tee -a /etc/log-create-user.log
+echo -e "Id        : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "alterId   : 0" | tee -a /etc/log-create-user.log
+echo -e "Security  : auto" | tee -a /etc/log-create-user.log
+echo -e "Network   : ws/grpc" | tee -a /etc/log-create-user.log
+echo -e "Path WS   : /vless" | tee -a /etc/log-create-user.log
+echo -e "Path GRPC : /vless-grpc" | tee -a /etc/log-create-user.log
+echo -e "----------------------------------" | tee -a /etc/log-create-user.log
+echo -e "Link TLS       : ${vlesslink1}" | tee -a /etc/log-create-user.log
+echo -e "----------------------------------" | tee -a /etc/log-create-user.log
+echo -e "Link none TLS  : ${vlesslink2}" | tee -a /etc/log-create-user.log
+echo -e "----------------------------------" | tee -a /etc/log-create-user.log
+echo -e "Link GRPC      : ${vlesslink3}" | tee -a /etc/log-create-user.log
+echo -e "----------------------------------" | tee -a /etc/log-create-user.log
+echo -e "Expired On : ${exp}" | tee -a /etc/log-create-user.log
+echo -e "----------------------------------" | tee -a /etc/log-create-user.log
+echo "" | tee -a /etc/log-create-user.log
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu
