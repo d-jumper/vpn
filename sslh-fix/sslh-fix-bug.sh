6769c2c32251b2caa6d0b7c29b55e4ea71af968d
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-09-29 13:10:31 +0700

    Delete sslh-fix directory

--- sslh-fix/sslh-fix-bug.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# ==========================================
-echo "5 4 * * * root clearlog && sslh-fix-reboot" >> /etc/crontab
-echo "5 5 * * * root clearlog && sslh-fix-reboot" >> /etc/crontab
