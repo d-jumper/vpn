e1ecd3c7134a88d0b726a47b41e1fe0b5de9da8c
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-10-02 19:13:21 +0700

    Add files via upload

--- xray/trojan/add-tr.sh
+++ xray/trojan/add-tr.sh
@@ -126,13 +126,13 @@ clear
     echo -e "${NC}${CYAN}            ───✶ Example Config & Link ✶─── $NC" | tee -a /etc/log-create-user.log
     echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/log-create-user.log
     echo -e "  ${RED}•${NC} ${CYAN}Link WS TLS       ➣ $NC" | tee -a /etc/log-create-user.log
-    echo -e "  ${RED}•${NC} ${CYAN}${trojanlink1} $NC" | tee -a /etc/log-create-user.log
+    echo -e "  ${NC}${CYAN}${trojanlink1} $NC" | tee -a /etc/log-create-user.log
     echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/log-create-user.log
     echo -e "  ${RED}•${NC} ${CYAN}Link NONE TLS     ➣ $NC" | tee -a /etc/log-create-user.log
-    echo -e "  ${RED}•${NC} ${CYAN}${trojanlink2} $NC" | tee -a /etc/log-create-user.log
+    echo -e "  ${NC}${CYAN}${trojanlink2} $NC" | tee -a /etc/log-create-user.log
     echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/log-create-user.log
     echo -e "  ${RED}•${NC} ${CYAN}Link GRPC         ➣ $NC" | tee -a /etc/log-create-user.log
-    echo -e "  ${RED}•${NC} ${CYAN}${trojanlink3} $NC" | tee -a /etc/log-create-user.log
+    echo -e "  ${NC}${CYAN}${trojanlink3} $NC" | tee -a /etc/log-create-user.log
     echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/log-create-user.log
     echo -e "" | tee -a /etc/log-create-user.log
     echo -e "${NC}${CYAN}              ───✶ Created - Expired ✶─── $NC" | tee -a /etc/log-create-user.log
