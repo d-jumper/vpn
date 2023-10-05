95dc7d69e513f9a723ac769be760b9b43c11ffa0
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-10-05 13:10:22 +0700

    Add files via upload

--- shadowsocks/shadowsocks.sh
+++ shadowsocks/shadowsocks.sh
@@ -96,5 +96,5 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save >> /etc/iptables.up.rules
 ip6tables-save >> /etc/ip6tables.up.rules
 echo ""
-echo -e "[ ${green}INFO$NC ] SETTING SHADOWSOCKS SUKSES !!!"
-sleep 1
+echo -e "[ ${green}INFO$NC ] INSTALLING SHADOWSOCKS-LIBEV SUCCESSFULLY !!!"
+sleep 5
