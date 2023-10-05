f9f3a7af5a1cefaa61e6fb6d870f2ccc7430a4f5
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-10-05 18:18:27 +0700

    Add files via upload

--- trojan-go/add-trgo.sh
+++ trojan-go/add-trgo.sh
@@ -46,7 +46,7 @@ IP=$(cat ${arfvpn}/IP)
 domain=$(cat ${arfvpn}/domain)
 clear 
 
-trgo="$(cat ~/log-install.txt | grep -w "Trojan GO " | cut -d: -f2|sed 's/ //g')"
+trgo="$(cat /etc/arfvpn/log-install.txt | grep -w "Trojan GO " | cut -d: -f2|sed 's/ //g')"
 until [[ ${user} =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 
     echo -e "\033[0;34m┌─────────────────────────────────────────────────────┐${NC}"
@@ -95,32 +95,32 @@ systemctl restart trojan-go.service
 trojangolink="trojan-go://${user}@${domain}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/trojan-go&encryption=none#${user}"
 clear
         
-echo -e "" | tee -a /etc/log-create-user.log
-echo -e "" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m┌─────────────────────────────────────────────────────┐${NC}" | tee -a /etc/log-create-user.log
-echo -e "               ⇱ \e[32;1m✶ Add Trojan-Go Account ✶\e[0m ⇲ ${NC}" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m└─────────────────────────────────────────────────────┘${NC}" | tee -a /etc/log-create-user.log
-echo -e "" | tee -a /etc/log-create-user.log
-echo -e "${NC}${CYAN}              ───✶ Trojan-GO Account ✶─── $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Remarks           : ${user} $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}IP/Host           : ${IP} $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Domain            : ${domain} $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Password          : ${user} $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Port GO           : ${trgo} $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Path GO           : /trojan-go $NC" | tee -a /etc/log-create-user.log
-echo -e "" | tee -a /etc/log-create-user.log
-echo -e "${NC}${CYAN}            ───✶ Example Config & Link ✶─── $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Link GO           ➣ $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${NC}${CYAN}${trojangolink} $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/log-create-user.log
-echo -e "" | tee -a /etc/log-create-user.log
-echo -e "${NC}${CYAN}              ───✶ Created - Expired ✶─── $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Created           : ${hariini} $NC" | tee -a /etc/log-create-user.log
-echo -e "  ${RED}•${NC} ${CYAN}Expired On        : ${exp} $NC" | tee -a /etc/log-create-user.log
-echo -e "" | tee -a /etc/log-create-user.log
-echo -e "\033[0;34m└─────────────────────────────────────────────────────┘${NC}" | tee -a /etc/log-create-user.log
-echo "" | tee -a /etc/log-create-user.log
-echo "" | tee -a /etc/log-create-user.log 
+echo -e "" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "\033[0;34m┌─────────────────────────────────────────────────────┐${NC}" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "               ⇱ \e[32;1m✶ Add Trojan-Go Account ✶\e[0m ⇲ ${NC}" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "\033[0;34m└─────────────────────────────────────────────────────┘${NC}" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "${NC}${CYAN}              ───✶ Trojan-GO Account ✶─── $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Remarks           : ${user} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}IP/Host           : ${IP} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Domain            : ${domain} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Password          : ${user} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Port GO           : ${trgo} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Path GO           : /trojan-go $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "${NC}${CYAN}            ───✶ Example Config & Link ✶─── $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Link GO           ➣ $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${NC}${CYAN}${trojangolink} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}────────────────────────────────── $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "${NC}${CYAN}              ───✶ Created - Expired ✶─── $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Created           : ${hariini} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "  ${RED}•${NC} ${CYAN}Expired On        : ${exp} $NC" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "" | tee -a /etc/arfvpn/log-create-user.log
+echo -e "\033[0;34m└─────────────────────────────────────────────────────┘${NC}" | tee -a /etc/arfvpn/log-create-user.log
+echo "" | tee -a /etc/arfvpn/log-create-user.log
+echo "" | tee -a /etc/arfvpn/log-create-user.log 
 read -n 1 -s -r -p "Press any key to back on menu"
 menu
