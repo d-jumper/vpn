b2cd15a5a86d6cdd18fad2afe139c392fc6a4c4a
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-10-09 19:06:54 +0700

    Add files via upload

--- nginx/nginx-server.sh
+++ nginx/nginx-server.sh
@@ -1,16 +1,61 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-tyblue='\e[1;36m'
-NC='\e[0m'
-purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
-tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-clear
+#########################################################
+# Export Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+TYBLUE='\e[1;36m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+NC='\033[0m'
 
+# Export Align
+BOLD="\e[1m"
+WARNING="${RED}\e[5m"
+UNDERLINE="\e[4m"
+
+# Export Banner Status Information
+EROR="[${RED} EROR ${NC}]"
+INFO="[${LIGHT} INFO ${NC}]"
+OK="[${LIGHT} OK ! ${NC}]"
+CEKLIST="[${LIGHT}✔${NC}]"
+PENDING="[${YELLOW} PENDING ${NC}]"
+SEND="[${GREEN} SEND ${NC}]"
+RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+#########################################################
+
+arfvpn_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+# Start
+echo -ne "     ${YELLOW}Processing ${NC}${LIGHT}- [${NC}"
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "${TYBLUE}>${NC}"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "${TYBLUE}]${NC}"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   # Finish
+   echo -ne "           ${YELLOW}Done ${NC}${LIGHT}- [${NC}"
+done
+echo -e "${LIGHT}] -${NC}${LIGHT} OK !${NC}"
+tput cnorm
+}
+
+#########################################################
 cd /root
 source /etc/os-release
 arfvpn="/etc/arfvpn"
@@ -23,14 +68,8 @@ IP=$(cat ${arfvpn}/IP)
 MYIP2="s/ipxxx/${IP}/g";
 clear
 
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "$green          INSTALLING NGINX SERVER $NC"
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-date
-sleep 3
-echo -e "[ ${green}INFO$NC ] INSTALLING NGINX SERVER"
-cd
-
+#########################################################
+installing_nginx () {
 systemctl stop nginx
 cd ${nginx}
 rm ${nginx}/nginx.conf
@@ -42,7 +81,10 @@ wget -O ${nginx}/sites-available/${domain}.conf "https://${github}/nginx/arfvps.
 sed -i "${MYIP2}" ${nginx}/sites-available/${domain}.conf
 sed -i "${DOMAIN2}" ${nginx}/sites-available/${domain}.conf
 sudo ln -s ${nginx}/sites-available/${domain}.conf ${nginx}/sites-enabled
+}
 
+#########################################################
+user_root () {
 useradd -m arfvps;
 mkdir -p ${arfvps}/
 cd ${arfvps}/
@@ -51,18 +93,39 @@ echo "<?php phpinfo() ?>" > ${arfvps}/info.php
 chown -R www-data:www-data ${arfvps}
 chmod -R g+rw ${arfvps}
 chmod +x /home/
-chmod +x /home/arfvps/
+chmod +x /home/arfvps/  
 chmod +x ${arfvps}/
+}
 
+#########################################################
+php_v () {
 cd
 ls /etc/php > phpversion
 phpv=$(cat /root/phpversion)
 sed -i "s/listen = \/run\/php\/php${phpv}-fpm.sock/listen = 127.0.0.1:9000/g" /etc/php/${phpv}/fpm/pool.d/www.conf
 rm /root/phpversion
+}
 
+#########################################################
+echo -e " ${LIGHT}- ${NC}Installing Nginx Server"
+arfvpn_bar 'installing_nginx'
+echo -e ""
+sleep 2
+
+echo -e " ${LIGHT}- ${NC}Create Root user"
+arfvpn_bar 'user_root'
+echo -e ""
+sleep 2
+
+echo -e " ${LIGHT}- ${NC}Set PHP-FPM"
+arfvpn_bar 'php_v'
+echo -e ""
+sleep 2
+
+#########################################################
+echo -e " ${LIGHT}- ${NC}Make a SSL CERT"
+sleep 2
 wget -O /usr/bin/cert "https://${github}/cert/cert.sh"
 chmod +x /usr/bin/cert
 sed -i -e 's/\r$//' /usr/bin/cert
 /usr/bin/cert
-
-sleep 5
