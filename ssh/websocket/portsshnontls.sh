8e5143828b2bd3bd8799684d4b8e79cdac636b8e
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-10-12 12:13:36 +0700

    Add files via upload

---
+++ ssh/websocket/portsshnontls.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+#########################################################
+# Export Color
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+BINC='\e[0m'
+
+RED='\033[0;31m'      # RED 1
+RED2='\e[1;31m'       # RED 2
+GREEN='\033[0;32m'   # GREEN 1
+GREEN2='\e[1;32m'    # GREEN 2
+STABILO='\e[32;1m'    # STABILO
+ORANGE='\033[0;33m' # ORANGE
+PURPLE='\033[0;35m'  # PURPLE
+BLUE='\033[0;34m'     # BLUE 1
+TYBLUE='\e[1;36m'     # BLUE 2
+CYAN='\033[0;36m'     # CYAN
+LIGHT='\033[0;37m'    # LIGHT
+NC='\033[0m'           # NC
+
+bl='\e[36;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+green_mix() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red_mix() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+# Export Align
+BOLD="\e[1m"
+WARNING="${RED}\e[5m"
+UNDERLINE="\e[4m"
+
+# Export Banner Status Information
+EROR="[${RED} EROR ${NC}]"
+INFO="[${LIGHT} INFO ${NC}]"
+OK="[${LIGHT} OK ! ${NC}]"
+CEKLIST="[${LIGHT}✔${NC}]"
+PENDING="[${YELLOW} PENDING ${NC}]"
+SEND="[${GREEN} SEND ${NC}]"
+RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+#########################################################
+arfvpn_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+# Start
+echo -ne "     ${ORANGE}Processing ${NC}${LIGHT}- [${NC}"
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "${TYBLUE}>${NC}"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "${LIGHT}]${NC}"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   # Finish
+   echo -ne "           ${ORANGE}Done ${NC}${LIGHT}- [${NC}"
+done
+echo -e "${LIGHT}] -${NC}${LIGHT} OK !${NC}"
+tput cnorm
+}
+
+#########################################################
+ws="$(cat /etc/arfvpn/log-install.txt | grep -w "Websocket None TLS" | cut -d: -f2|sed 's/ //g')"
+clear
+
+#input port untuk pengganti
+echo -e "======================================"
+echo -e ""
+echo -e "Change Port $ws"
+echo -e ""
+echo -e "======================================"
+read -p "New Port SSH WS : " ws2
+if [ -z $ws2 ]; then
+echo "Please Input Port"
+exit 0
+fi
+cek=$(netstat -nutlp | grep -w $ws2)
+if [[ -z $cek ]]; then
+
+
+#ganti port layanan
+sed -i "s/$ws/$ws2/g" /etc/default/sslh
+sed -i "s/   - Websocket None TLS      : $ws/   - Websocket None TLS      : $ws2/g" /etc/arfvpn/log-install.txt
+
+iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport $ws -j ACCEPT
+iptables -D INPUT -m state --state NEW -m udp -p udp --dport $ws -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $ws2 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport $ws2 -j ACCEPT
+iptables-save >> /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save > /dev/null
+netfilter-persistent reload > /dev/null
+
+
+sed -i "s/$ws/$ws2/g" /etc/systemd/system/ws-nontls.service
+systemctl restart sslh
+systemctl daemon-reload
+systemctl restart ws-nontls > /dev/null
+
+#ganti port info
+
+echo -e "\e[032;1mPort $ws2 modified successfully\e[0m"
+else
+echo "Port $ws2 is used"
+fi
