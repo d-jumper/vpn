a83debc2be86e28a5c45a903da2da0ccc60010a1
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-10-12 12:15:06 +0700

    Add files via upload

--- trojan-go/renew-trgo.sh
+++ trojan-go/renew-trgo.sh
@@ -1,36 +1,96 @@
 #!/bin/bash
+#########################################################
+# Export Color
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+BINC='\e[0m'
 
-# // Exporting Language to UTF-8
-export LC_ALL='en_US.UTF-8'
-export LANG='en_US.UTF-8'
-export LANGUAGE='en_US.UTF-8'
-export LC_CTYPE='en_US.utf8'
+RED='\033[0;31m'      # RED 1
+RED2='\e[1;31m'       # RED 2
+GREEN='\033[0;32m'   # GREEN 1
+GREEN2='\e[1;32m'    # GREEN 2
+STABILO='\e[32;1m'    # STABILO
+ORANGE='\033[0;33m' # ORANGE
+PURPLE='\033[0;35m'  # PURPLE
+BLUE='\033[0;34m'     # BLUE 1
+TYBLUE='\e[1;36m'     # BLUE 2
+CYAN='\033[0;36m'     # CYAN
+LIGHT='\033[0;37m'    # LIGHT
+NC='\033[0m'           # NC
 
-# // Export Color & Information
-export RED='\033[0;31m'
-export GREEN='\033[0;32m'
-export YELLOW='\033[0;33m'
-export BLUE='\033[0;34m'
-export PURPLE='\033[0;35m'
-export CYAN='\033[0;36m'
-export LIGHT='\033[0;37m'
-export NC='\033[0m'
+bl='\e[36;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+green_mix() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red_mix() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
-# // Export Banner Status Information
-export EROR="[${RED} EROR ${NC}]"
-export INFO="[${YELLOW} INFO ${NC}]"
-export OKEY="[${GREEN} OKEY ${NC}]"
-export PENDING="[${YELLOW} PENDING ${NC}]"
-export SEND="[${YELLOW} SEND ${NC}]"
-export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+# Export Align
+BOLD="\e[1m"
+WARNING="${RED}\e[5m"
+UNDERLINE="\e[4m"
 
-# / letssgoooo
+# Export Banner Status Information
+EROR="[${RED} EROR ${NC}]"
+INFO="[${LIGHT} INFO ${NC}]"
+OK="[${LIGHT} OK ! ${NC}]"
+CEKLIST="[${LIGHT}✔${NC}]"
+PENDING="[${YELLOW} PENDING ${NC}]"
+SEND="[${GREEN} SEND ${NC}]"
+RECEIVE="[${YELLOW} RECEIVE ${NC}]"
 
-# // Export Align
-export BOLD="\e[1m"
-export WARNING="${RED}\e[5m"
-export UNDERLINE="\e[4m"
+#########################################################
+arfvpn_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+# Start
+echo -ne "     ${ORANGE}Processing ${NC}${LIGHT}- [${NC}"
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "${TYBLUE}>${NC}"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "${LIGHT}]${NC}"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   # Finish
+   echo -ne "           ${ORANGE}Done ${NC}${LIGHT}- [${NC}"
+done
+echo -e "${LIGHT}] -${NC}${LIGHT} OK !${NC}"
+tput cnorm
+}
 
+#########################################################
 # // Root Checking
 if [ "${EUID}" -ne 0 ]; then
 		echo -e "${EROR} Please Run This Script As Root User !"
@@ -110,8 +170,15 @@ clear
     exp2=$(( (d1 - d2) / 86400 ))
     exp3=$((${exp2} + ${masaaktif}))
     exp4=`date -d "${exp3} days" +"%Y-%m-%d"`
+    renewtrgo () {
     sed -i "s/#trgo# ${user} ${exp}/#trgo# ${user} ${exp4}/g" /etc/arfvpn/trojan-go/akun.conf
     systemctl restart trojan-go > /dev/null 2>&1
+    }
+    echo -e " ${LIGHT}- ${NC}Renew User Trojan-Go"
+    arfvpn_bar 'renewtrgo'
+    echo -e ""
+    sleep 2
+    history -c
     clear
     
     echo -e "\033[0;34m┌─────────────────────────────────────────────────────┐${NC}"
@@ -124,5 +191,8 @@ clear
     echo -e ""
     echo -e "\033[0;34m└─────────────────────────────────────────────────────┘${NC}"
     echo ""
-    read -n 1 -s -r -p "Press any key to back on menu"
+    echo -e "     ${LIGHT}Press ${NC}[ ENTER ]${LIGHT} to ${NC}${BIYellow}Back to Menu${NC}${LIGHT} or ${NC}${RED}CTRL+C${NC}${LIGHT} to exit${NC}"
+    read -p ""
+    history -c
+    clear
     menu
--- trojan-go/trojan-go.sh
+++ trojan-go/trojan-go.sh
@@ -1,15 +1,96 @@
 #!/bin/bash
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-tyblue='\e[1;36m'
-NC='\e[0m'
-purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
-tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
-yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+#########################################################
+# Export Color
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+BINC='\e[0m'
 
+RED='\033[0;31m'      # RED 1
+RED2='\e[1;31m'       # RED 2
+GREEN='\033[0;32m'   # GREEN 1
+GREEN2='\e[1;32m'    # GREEN 2
+STABILO='\e[32;1m'    # STABILO
+ORANGE='\033[0;33m' # ORANGE
+PURPLE='\033[0;35m'  # PURPLE
+BLUE='\033[0;34m'     # BLUE 1
+TYBLUE='\e[1;36m'     # BLUE 2
+CYAN='\033[0;36m'     # CYAN
+LIGHT='\033[0;37m'    # LIGHT
+NC='\033[0m'           # NC
+
+bl='\e[36;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+green_mix() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red_mix() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+# Export Align
+BOLD="\e[1m"
+WARNING="${RED}\e[5m"
+UNDERLINE="\e[4m"
+
+# Export Banner Status Information
+EROR="[${RED} EROR ${NC}]"
+INFO="[${LIGHT} INFO ${NC}]"
+OK="[${LIGHT} OK ! ${NC}]"
+CEKLIST="[${LIGHT}✔${NC}]"
+PENDING="[${YELLOW} PENDING ${NC}]"
+SEND="[${GREEN} SEND ${NC}]"
+RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+
+#########################################################
+arfvpn_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+# Start
+echo -ne "     ${ORANGE}Processing ${NC}${LIGHT}- [${NC}"
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "${TYBLUE}>${NC}"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "${LIGHT}]${NC}"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   # Finish
+   echo -ne "           ${ORANGE}Done ${NC}${LIGHT}- [${NC}"
+done
+echo -e "${LIGHT}] -${NC}${LIGHT} OK !${NC}"
+tput cnorm
+}
+
+#########################################################
 arfvpn="/etc/arfvpn"
 trgo="/etc/arfvpn/trojan-go"
 logtrgo="/var/log/arfvpn/trojan-go"
@@ -27,6 +108,8 @@ echo -e "[ ${green}INFO$NC ] INSTALLING TROJAN-GO"
 sleep 1
 latest_version="$(curl -s "https://api.github.com/repos/p4gefau1t/trojan-go/releases" | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 trojango_link="https://github.com/p4gefau1t/trojan-go/releases/download/v${latest_version}/trojan-go-linux-amd64.zip"
+
+dirtrgo () {
 mkdir -p "/usr/bin/trojan-go"
 #mkdir -p "${trgo}"
 cd `mktemp -d`
@@ -37,10 +120,16 @@ chmod +x /usr/local/bin/trojan-go
 mkdir -p ${logtrgo}
 touch ${trgo}/akun.conf
 touch ${logtrgo}/trojan-go.log
+}
+echo -e " ${LIGHT}- ${NC}Creating Directory Trojan-Go"
+arfvpn_bar 'dirtrgo'
+echo -e ""
+sleep 2
 
 # Buat Config Trojan Go
-echo -e "[ ${green}INFO$NC ] MEMBUAT CONFIG TROJAN-GO"
-sleep 1
+config_trgo () {
+#echo -e "[ ${green}INFO$NC ] MEMBUAT CONFIG TROJAN-GO"
+#sleep 1
 cat > ${trgo}/config.json << END
 {
   "run_type": "server",
@@ -129,7 +218,13 @@ END
 cat > ${trgo}/uuid << END
 ${uuid}
 END
+}
+echo -e " ${LIGHT}- ${NC}Creating Config Trojan-Go"
+arfvpn_bar 'config_trgo'
+echo -e ""
+sleep 2
 
+script_trgo () {
 wget -q -O /usr/bin/add-trgo "https://${github}/trojan-go/add-trgo.sh" && chmod +x /usr/bin/add-trgo
 wget -q -O /usr/bin/cek-trgo "https://${github}/trojan-go/cek-trgo.sh" && chmod +x /usr/bin/cek-trgo
 wget -q -O /usr/bin/del-trgo "https://${github}/trojan-go/del-trgo.sh" && chmod +x /usr/bin/del-trgo
@@ -139,6 +234,12 @@ sed -i -e 's/\r$//' /bin/add-trgo
 sed -i -e 's/\r$//' /bin/cek-trgo
 sed -i -e 's/\r$//' /bin/del-trgo
 sed -i -e 's/\r$//' /bin/renew-trgo
+}
+echo -e " ${LIGHT}- ${NC}Installing Script Trojan-Go"
+arfvpn_bar 'script_trgo'
+echo -e ""
+sleep 2
 
-echo -e "[ ${green}INFO$NC ] INSTALLING TROJAN-GO SUCCESSFULLY !!!"
-sleep 5
+echo -e " ${OK} Installing Trojan-go Successfully !!! ${CEKLIST}"
+echo -e ""
+sleep 2
