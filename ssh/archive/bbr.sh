10f3280dc04bf4a876f3d7c0256a2ef958f57800
arfprsty810 <94728173+arfprsty810@users.noreply.github.com>
2023-10-13 13:04:52 +0700

    Add files via upload

--- ssh/archive/bbr.sh
+++ ssh/archive/bbr.sh
@@ -1,19 +1,114 @@
 #!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-#Getting
+#########################################################
+# Export Color
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+UWhite='\033[4;37m'       # White
+On_IPurple='\033[0;105m'  #
+On_IRed='\033[0;101m'
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+BINC='\e[0m'
 
+RED='\033[0;31m'      # RED 1
+RED2='\e[1;31m'       # RED 2
+GREEN='\033[0;32m'   # GREEN 1
+GREEN2='\e[1;32m'    # GREEN 2
+STABILO='\e[32;1m'    # STABILO
+ORANGE='\033[0;33m' # ORANGE
+PURPLE='\033[0;35m'  # PURPLE
+BLUE='\033[0;34m'     # BLUE 1
+TYBLUE='\e[1;36m'     # BLUE 2
+CYAN='\033[0;36m'     # CYAN
+LIGHT='\033[0;37m'    # LIGHT
+NC='\033[0m'           # NC
+
+bl='\e[36;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+green_mix() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red_mix() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+# Export Align
+BOLD="\e[1m"
+WARNING="${RED}\e[5m"
+UNDERLINE="\e[4m"
+
+# Export Banner Status Information
+EROR="[${RED} EROR ${NC}]"
+INFO="[${LIGHT} INFO ${NC}]"
+OK="[${LIGHT} OK ! ${NC}]"
+CEKLIST="[${LIGHT}✔${NC}]"
+PENDING="[${YELLOW} PENDING ${NC}]"
+SEND="[${GREEN} SEND ${NC}]"
+RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+SUCCESS="[${LIGHT} ✔ SUCCESS ✔ ${NC}]"
+
+#########################################################
+source /etc/os-release
+cd /root
+# // Root Checking
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${EROR} Please Run This Script As Root User !"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+
+#########################################################
+arfvpn_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+ tput civis
+# Start
+echo -ne "     ${ORANGE}Processing ${NC}${LIGHT}- [${NC}"
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "${TYBLUE}>${NC}"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "${LIGHT}]${NC}"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   # Finish
+   echo -ne "           ${ORANGE}Done ${NC}${LIGHT}- [${NC}"
+done
+echo -e "${LIGHT}] -${NC}${LIGHT} OK !${NC}"
+tput cnorm
+}
+
+#########################################################
+arfvpn="/etc/arfvpn"
+github=$(cat ${arfvpn}/github)
 clear
-#Optimasi Speed By Akbar Maulana
+
 Add_To_New_Line(){
 	if [ "$(tail -n1 $1 | wc -l)" == "0"  ];then
 		echo "" >> "$1"
@@ -28,30 +123,22 @@ Check_And_Add_Line(){
 }
 
 Install_BBR(){
-echo "#############################################"
-echo "Install TCP_BBR..."
 if [ -n "$(lsmod | grep bbr)" ];then
-echo "TCP_BBR sudah diinstall."
-echo "#############################################"
 return 1
 fi
-echo "Mulai menginstall TCP_BBR..."
 modprobe tcp_bbr
 Add_To_New_Line "/etc/modules-load.d/modules.conf" "tcp_bbr"
 Add_To_New_Line "/etc/sysctl.conf" "net.core.default_qdisc = fq"
 Add_To_New_Line "/etc/sysctl.conf" "net.ipv4.tcp_congestion_control = bbr"
 sysctl -p
 if [ -n "$(sysctl net.ipv4.tcp_available_congestion_control | grep bbr)" ] && [ -n "$(sysctl net.ipv4.tcp_congestion_control | grep bbr)" ] && [ -n "$(lsmod | grep "tcp_bbr")" ];then
-	echo "TCP_BBR Install Success."
+	sleep 1
 else
-	echo "Gagal menginstall TCP_BBR."
+	sleep 2
 fi
-echo "#############################################"
 }
 
 Optimize_Parameters(){
-echo "#############################################"
-echo "Optimasi Parameters..."
 Check_And_Add_Line "/etc/security/limits.conf" "* soft nofile 51200"
 Check_And_Add_Line "/etc/security/limits.conf" "* hard nofile 51200"
 Check_And_Add_Line "/etc/security/limits.conf" "root soft nofile 51200"
@@ -73,9 +160,23 @@ Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_mem = 25600 51200 102400"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_rmem = 4096 87380 67108864"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_wmem = 4096 65536 67108864"
 Check_And_Add_Line "/etc/sysctl.conf" "net.ipv4.tcp_mtu_probing = 1"
-echo "Optimasi Parameters Selesai."
-echo "#############################################"
 }
-Install_BBR
-Optimize_Parameters
-rm -f /root/bbr.sh
+
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "          INSTALLING BBR"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " ${LIGHT}- ${NC}Installing BBR"
+arfvpn_bar 'Install_BBR'
+echo -e ""
+sleep 2
+
+echo -e " ${LIGHT}- ${NC}Optimize_Parameters"
+arfvpn_bar 'Optimize_Parameters'
+echo -e ""
+sleep 2
+
+echo -e " ${OK} Installing BBR Successfully !!! ${CEKLIST}"
+echo -e ""
+sleep 2
+clear
--- ssh/archive/set.sh
+++ ssh/archive/set.sh
@@ -1,9 +1,4 @@
-
-
-
-
-
-
+#!/bin/bash
 
 <!DOCTYPE html>
 <html lang="en" data-color-mode="auto" data-light-theme="light" data-dark-theme="dark" data-a11y-animated-images="system">
